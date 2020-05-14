5e34f6392b84f94cbb73e9fbe1fcbc4ec9827259
coryortega <coryortega3@gmail.com>
2020-05-14 16:17:04 -0700

    changed some stuff

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const twit=require('twit')
 var path = require('path')
+const config = require('./config.js');
 const axios = require('axios')
 const https = require('https')
 var express = require('express');
@@ -17,12 +18,12 @@ app.set('port', process.env.PORT || 5000);
 //   require('./.env');
 // }
 
-var config = {
-  consumer_key:         process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
-  access_token:         process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
-}
+// var config = {
+//   consumer_key:         process.env.TWITTER_CONSUMER_KEY,
+//   consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
+//   access_token:         process.env.TWITTER_ACCESS_TOKEN,
+//   access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
+// }
 
 const T=new twit(config)
 
