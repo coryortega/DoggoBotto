2f6119bf014e67d92de646a2fda90f953a4933ee
coryortega <coryortega3@gmail.com>
2020-05-15 14:33:44 -0700

    it should work

--- bot.js
+++ bot.js
@@ -1,85 +1,81 @@
-const fs = require('fs')
-const twit=require('twit')
-var path = require('path')
+const fs = require('fs');
+const twit=require('twit');
+var path = require('path');
 const config = require('./config.js');
-const axios = require('axios')
-const https = require('https')
+const axios = require('axios');
 var express = require('express');
 var request  = require('request').defaults({ encoding: null });
-var http = require('http');
-var {Base64Encode} = require('base64-stream');
-const T=new twit(config)
+var schedule = require('node-schedule');
+
+const T=new twit(config);
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
-// app.set('port', 5000);
 
-
-// if(process.env.TWITTER_CONSUMER_KEY == undefined){
-//   require('./.env');
-// }
-
-// var config = {
-//   consumer_key:         process.env.TWITTER_CONSUMER_KEY,
-//   consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
-//   access_token:         process.env.TWITTER_ACCESS_TOKEN,
-//   access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
-// }
-
-
-
-var now = new Date();
-
-let i = 2
-
-var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 0, 0, 0) - now;
-
-// fs.writeFile(`/dogpic/dog${i + 1}.jpg`, 'temp', (err) => {
-//   if (err) {
-//     console.error(err)
-//     return
-//   }
-//   //file written successfully
-//   console.log('file written')
-// })
+// i is used to keep track of the Doggo pics stored on the server. Starts at 2 because the program starts with 2 Doggo pics preloaded
+i = 2
 
 var download = function(uri, filename, callback){
   request.head(uri, function(err, res, body){
     console.log('content-type:', res.headers['content-type']);
     console.log('content-length:', res.headers['content-length']);
-
     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
   });
 };
+
+function getDoggoQuote(){
+  return axios
+  .get("https://type.fit/api/quotes")
+  .then(response => {
+
+  return item = response.data[Math.floor(Math.random() * response.data.length)];
+
+  })
+  .catch(error => {
+    console.log("The data was not returned", error);
+  });
+}
 
 function postDoggo(base64){
 
   console.log('Uploading an image...');
 
-  T.post('media/upload', { media_data: base64 }, function (err, data, response) {
-    if (err){
-      console.log('ERROR:');
-      console.log(err);
-    }
-    else{
-      console.log('Image uploaded!');
-      console.log('Now tweeting it...');
+  getDoggoQuote().then(data => {
 
-      T.post('statuses/update', {
-        media_ids: new Array(data.media_id_string)
-      },
-        function(err, data, response) {
-          if (err){
-            console.log('ERROR:');
-            console.log(err);
-          }
-          else{
-            console.log('Posted an image!');
-          }
-        }
-      );
+    doggoQuote = data
+
+    if(doggoQuote.author == 'null'){
+      doggoQuote.author = 'Unknown Author'
     }
-  });
+  
+    T.post('media/upload', { media_data: base64 }, function (err, data, response) {
+      if (err){
+        console.log('ERROR:');
+        console.log(err);
+      }
+      else{
+        console.log('Image uploaded!');
+        console.log('Now tweeting it...');  
+  
+        T.post('statuses/update', {
+          media_ids: new Array(data.media_id_string),
+          status:`${doggoQuote.text} - ${doggoQuote.author}`
+        },
+          function(err, data, response) {
+            if (err){
+              console.log('ERROR:');
+              console.log(err);
+            }
+            else{
+              console.log('Posted an image!');
+            }
+          }
+        );
+      }
+    });    
+
+  })    
+
 }
 
 function uploadDoggo(){
@@ -99,86 +95,23 @@ function uploadDoggo(){
           b64content = fs.readFileSync(image_path, { encoding: 'base64' });
 
       postDoggo(b64content)
-
-        // console.log('Uploading an image...');
-
-        // T.post('media/upload', { media_data: string }, function (err, data, response) {
-        //   if (err){
-        //     console.log('ERROR:');
-        //     console.log(err);
-        //   }
-        //   else{
-        //     console.log('Image uploaded!');
-        //     console.log('Now tweeting it...');
-      
-        //     T.post('statuses/update', {
-        //       media_ids: new Array(data.media_id_string)
-        //     },
-        //       function(err, data, response) {
-        //         if (err){
-        //           console.log('ERROR:');
-        //           console.log(err);
-        //         }
-        //         else{
-        //           console.log('Posted an image!');
-        //         }
-        //       }
-        //     );
-        //   }
-        // });
-    //     }
-    // });
-
-    // const request = https.get(response.data.message, function(response) {
-    //   response.pipe(wstream)
-    // })
-
   })
-.catch(error => {
-  console.log("The data was not returned", error);
+  .catch(error => {
+    console.log("The data was not returned", error);
+  });
+
+}
+
+
+// setInterval(function(){uploadDoggo()}, 10000)
+
+
+// var j = schedule.scheduleJob('12 * * * *', uploadDoggo());
+
+var j = schedule.scheduleJob('15 * * * *', function(){
+  uploadDoggo();
 });
 
-  // var image_path = path.join(__dirname, `/dogpic/dog${i}.jpg`),
-  //     b64content = fs.readFileSync(image_path, { encoding: 'base64' });
-
-
-  // console.log('Uploading an image...');
-
-  // T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-  //   if (err){
-  //     console.log('ERROR:');
-  //     console.log(err);
-  //   }
-  //   else{
-  //     console.log('Image uploaded!');
-  //     console.log('Now tweeting it...');
-
-  //     T.post('statuses/update', {
-  //       media_ids: new Array(data.media_id_string)
-  //     },
-  //       function(err, data, response) {
-  //         if (err){
-  //           console.log('ERROR:');
-  //           console.log(err);
-  //         }
-  //         else{
-  //           console.log('Posted an image!');
-  //         }
-  //       }
-  //     );
-  //   }
-  // });
-
-}
-
-if (millisTill10 < 0) {
-     millisTill10 += 86400000;
-}
-
-
-setInterval(function(){uploadDoggo()}, 10000)
-// setTimeout(function(){uploadDoggo()}, 5000);
-// setTimeout(function(){uploadDoggo()}, millisTill10);
 
 app.listen(app.get('port'), function() {
   console.log('Bot is running on port', app.get('port'));
--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-// if(process.env.TWITTER_CONSUMER_KEY == undefined){
-//     require('./.env');
-//   }
+if(process.env.TWITTER_CONSUMER_KEY == undefined){
+    require('./.env');
+  }
 
-// module.exports = {
-//     consumer_key:         process.env.CONSUMER_KEY,
-//     consumer_secret:      process.env.CONSUMER_SECRET,
-//     access_token:         process.env.ACCESS_TOKEN,
-//     access_token_secret:  process.env.ACCESS_TOKEN_SECRET
-//   }
+module.exports = {
+    consumer_key:         process.env.CONSUMER_KEY,
+    consumer_secret:      process.env.CONSUMER_SECRET,
+    access_token:         process.env.ACCESS_TOKEN,
+    access_token_secret:  process.env.ACCESS_TOKEN_SECRET
+  }
