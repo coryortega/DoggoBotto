b9d1acf872c108adfa3769e5b58cb0fe5595d858
coryortega <coryortega3@gmail.com>
2020-05-15 11:22:33 -0700

    test

--- config.js
+++ config.js
@@ -2,9 +2,9 @@
 //     require('./.env');
 //   }
 
-module.exports = {
-    consumer_key:         process.env.CONSUMER_KEY,
-    consumer_secret:      process.env.CONSUMER_SECRET,
-    access_token:         process.env.ACCESS_TOKEN,
-    access_token_secret:  process.env.ACCESS_TOKEN_SECRET
-  }
+// module.exports = {
+//     consumer_key:         process.env.CONSUMER_KEY,
+//     consumer_secret:      process.env.CONSUMER_SECRET,
+//     access_token:         process.env.ACCESS_TOKEN,
+//     access_token_secret:  process.env.ACCESS_TOKEN_SECRET
+//   }
