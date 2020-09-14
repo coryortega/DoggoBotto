74d78e9ca8ac2027be7f484aa928742bc5b19af0
coryortega <coryortega3@gmail.com>
2020-09-13 18:44:24 -0700

    added ability to upload doggos

--- bot.js
+++ bot.js
@@ -20,6 +20,18 @@ app.get("/post", function (req, res) {
   res.send("Posting Doggo!"), uploadDoggo();
 });
 
+app.get("/doggoOTD", function (req, res) {
+  getDoggoOTD().then((response) => {
+    console.log(response.status)
+    if(response.status === 204){
+      res.send("Non-user submitted Doggo"), uploadDoggo();
+    } else {
+      uploadUserDoggo(response)
+      res.send('yes')
+    }
+  })
+});
+
 app.get("/search", function (req, res) {
   res.send("hi");
   search.botSearch();
@@ -28,13 +40,29 @@ app.get("/search", function (req, res) {
   }, 30000);
 });
 
+app.post("/submit", function (req, res) {
+  const { img, caption, name, username } = req.body
+  res.send(`doggo: ${ img, caption, name, username }`)
+});
+
 var download = function (uri, filename, callback) {
   request.head(uri, function (err, res, body) {
-    console.log("content-type:", res.headers["content-type"]);
-    console.log("content-length:", res.headers["content-length"]);
+    // console.log("content-type:", res.headers["content-type"]);
+    // console.log("content-length:", res.headers["content-length"]);
     request(uri).pipe(fs.createWriteStream(filename)).on("close", callback);
   });
 };
+
+function getDoggoOTD() {
+  return axios
+    .get("https://doggobase.herokuapp.com/api/posts/images/verified")
+    .then((response) => {
+      return response
+    })
+    .catch((error) => {
+      console.log("The data was not returned", error);
+    });
+}
 
 function getDoggoQuote() {
   return axios
@@ -95,12 +123,60 @@ function postDoggo(base64) {
               console.log(err);
             } else {
               console.log("Posted an image!");
+              // console.log(Object.keys(response))
+              doggoOTD = data.id_str
+              console.log(doggoOTD)
             }
           }
         );
       }
     });
   });
+}
+
+function postUserDoggo(base64, userDoggo) {
+  console.log("Uploading an image...");
+
+    T.post("media/upload", { media_data: base64 }, function (
+      err,
+      data,
+      response
+    ) {
+      if (err) {
+        console.log("ERROR:");
+        console.log(err);
+      } else {
+        console.log("Image uploaded!");
+        console.log("Now tweeting it...");
+
+        T.post(
+          "statuses/update",
+          {
+            media_ids: new Array(data.media_id_string),
+            status:
+            `
+            ⭐Doggo of the Day!⭐${userDoggo.data.caption == null ? '' : '\n\n'+userDoggo.data.caption}\n\nDoggo name: ${userDoggo.data.name}\nSubmitted by: ${userDoggo.data.username == 'null' ? 'anonymous' : '@'+userDoggo.data.username}\n\nSubmit your doggo at https://doggobotto.com/!
+            `,
+          },
+          function (err, data, response) {
+            if (err) {
+              console.log("ERROR:");
+              console.log(err);
+            } else {
+              axios
+              .put(`https://doggobase.herokuapp.com/api/posts/images/${userDoggo.data.id}`, { posted: true, twitter_id: data.id_str })
+              .then((response) => {
+                console.log(response)
+              })
+              .catch((error) => {
+                console.error("Server Error", error);
+              });
+              console.log("Posted an image!");
+            }
+          }
+        );
+      }
+    });
 }
 
 function uploadDoggo() {
@@ -112,6 +188,16 @@ function uploadDoggo() {
         b64content = fs.readFileSync(image_path, { encoding: "base64" });
       postDoggo(b64content);
     });
+  });
+}
+
+function uploadUserDoggo(userDoggo) {
+  console.log("Opening...", userDoggo.data);
+
+  download(userDoggo.data.img, `./dogpic/doggo1.jpg`, function () {
+    var image_path = path.join(__dirname, `/dogpic/doggo1.jpg`),
+      b64content = fs.readFileSync(image_path, { encoding: "base64" });
+    postUserDoggo(b64content, userDoggo);
   });
 }
 
--- config.js
+++ config.js
@@ -3,8 +3,8 @@ if(process.env.CONSUMER_KEY == undefined){
   }
 
 module.exports = {
-    consumer_key:         process.env.CONSUMER_KEY,
-    consumer_secret:      process.env.CONSUMER_SECRET,
-    access_token:         process.env.ACCESS_TOKEN,
-    access_token_secret:  process.env.ACCESS_TOKEN_SECRET
+    consumer_key:         process.env.TEST_CONSUMER_KEY,
+    consumer_secret:      process.env.TEST_CONSUMER_SECRET,
+    access_token:         process.env.TEST_ACCESS_TOKEN,
+    access_token_secret:  process.env.TEST_ACCESS_TOKEN_SECRET
   }
